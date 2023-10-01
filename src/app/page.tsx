4387d4d440e4947da824f840374ce50db5da07ac
Aliaksandr Matveichykau <kobuqa@gmail.com>
2023-10-01 23:18:52 +0300

    init: project initialization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,97 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <main className="px-20 relative h-screen scroll-smooth overflow-auto">
+      <nav className="sticky top-0 z-10 bg-black py-5">
+        <ul className="flex py-5 gap-10">
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#investors">Investors</a>
+          </li>
+          <li>
+            <a href="#contacts">Contacts</a>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <p className="text-5xl text-center">Blockchain Solutions</p>
+      </div>
+      <section id="about" className="mt-14">
+        <p className="text-2xl">
+          At Blockchain Solutions, we are dedicated to revolutionizing
+          industries through the application of cutting-edge blockchain
+          technology. With expertise in developing custom-built blockchain
+          solutions, decentralized applications (DApps), and smart contracts, we
+          empower businesses to streamline their processes, enhance security,
+          and unlock unprecedented opportunities for growth
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      </section>
+      <section id="services" className="mt-12">
+        <p className="text-2xl">
+          One of our primary objectives is to establish a groundbreaking
+          e-commerce platform that fully embraces the potential of
+          cryptocurrencies, particularly Bitcoin. By providing a seamless
+          integration of Bitcoin as the primary currency, we aim to create a
+          platform that offers unparalleled convenience, security, and
+          accessibility to online shoppers worldwide.
+        </p>
+      </section>
+      <section id="investors" className="mt-12 flex flex-col gap-6">
+        <h3 className="text-4xl">Why invest in Blockchain Solutions</h3>
+        <h5 className="text-2xl">
+          Investing in Blockchain Solutions offers a unique opportunity to
+          participate in a rapidly growing market, capitalize on the potential
+          of blockchain technology, and secure high returns. Don't miss out on
+          being part of the revolution. Invest in Blockchain Solutions today!
+        </h5>
+        <h3 className="text-4xl">Services</h3>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <ul className="grid grid-cols-2 gap-6">
+          <li className="flex-col flex ">
+            <p className="font-bold text-2xl">Revolutionary Technology</p>
+            <h4>
+              Blockchain Solutions is at the forefront of the blockchain
+              revolution.
+            </h4>
+          </li>
+          <li className="flex flex-col">
+            <p className="font-bold text-2xl">Potential for High Returns</p>
+            <h4>
+              The blockchain industry is experiencing rapid growth, and with
+              that comes significant investment opportunities.
+            </h4>
+          </li>
+          <li className="flex flex-col">
+            <p className="font-bold text-2xl">Trusted Expertise</p>
+            <h4>
+              With years of experience in the blockchain industry, our team of
+              experts has successfully delivered numerous blockchain projects
+              across various sectors.
+            </h4>
+          </li>
+          <li>
+            <p className="font-bold text-2xl">Diversification Strategy</p>
+            <h4>
+              Investing in Blockchain Solutions allows you to diversify your
+              investment portfolio.
+            </h4>
+          </li>
+        </ul>
+      </section>
+      <footer id="contacts" className="mt-12">
+        <ul className="py-4">
+          <li>milo@gmail.com</li>
+          <li>tel.: 1-023912-312</li>
+          <li>
+            LLC Blockchain Solutions, Varšavská 715/36, Vinohrady, 120 00 Praha
+            2, Company ID number:195 23 441
+          </li>
+        </ul>
+      </footer>
     </main>
-  )
+  );
 }
